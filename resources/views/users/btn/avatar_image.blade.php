979df0bef5632850ac4ba09406dbd376c891b203
HossamSalaheldeen <hossam.salah1995@gmail.com>
2021-03-30 01:23:38 +0200

    adjust readme file

--- resources/views/users/btn/avatar_image.blade.php
+++ resources/views/users/btn/avatar_image.blade.php
@@ -1,1 +1,5 @@
+@if($avatar_image == 'avatar.png')
+<img src="{{ url('images/'. $avatar_image) }}" width="80" style="height: 45px" class="img-thumbnail" alt="user image">    
+@else
 <img src="{{ url('storage/users_images/'. $avatar_image) }}" width="80" style="height: 45px" class="img-thumbnail" alt="user image">
+@endif
