0562a81cf2067c8b7ae6d36d6de8fa0d266f9edd
mohammedegila <mohammedegila@gmail.com>
2021-03-31 19:45:02 +0200

    Add api to get all available rooms & sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasApiTokens,HasFactory, Notifiable, HasRoles;
+  
 
     /**
      * The attributes that are mass assignable.
